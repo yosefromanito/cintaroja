73452532f15982adda36f632a1f581f999473f02
yosefromanito <yosefromano@MacBook-Air-de-Yosef.local>
2020-07-09 18:33:17 -0500

    hola eli

--- src/Components/pages/Israelnews/Israelnews.js
+++ src/Components/pages/Israelnews/Israelnews.js
@@ -1,26 +1,40 @@
 import React,{useEffect, useState} from 'react'
 import Header from '../../Header/Header'
 import axios from 'axios'
+import Plantilla from '../plantilla/Plantilla'
+import './Israelnews.css'
 function Israelnews() {
-    
     const[news, getNews]=useState([])
         const url =`http://newsapi.org/v2/top-headlines?country=il&apiKey=dfb4be6513f44f54aae2a7b47999a581`
         useEffect(()=>{
         axios.get(url)
     .then((res)=>{
-    
-        console.log(res.Json)
-        var todo=res.Json
-        getNews(todo)
+        console.log(res.data)
+        var todo=res.data
+        getNews(todo.articles)
     })
     .catch((err)=>{
         console.log(err)
     });
-        },[])
+    },[])
     return (
         <div>
 <Header/>
-            <h1>ESTAS SON LAS NOTICIAS DE ISRAEL</h1>
+            <h1 style={{textAlign:'center'}}>TOP NEWS</h1>
+           <div className="row">
+            {news.map((noticias)=>{
+             return <Plantilla 
+             titulo={noticias.title}
+             contexto={noticias.description}
+             referencia={noticias.source.name}
+             imagen={noticias.urlToImage}
+             url={noticias.url}
+             />
+             
+  
+            })
+            }
+               </div> 
         </div>
     )
 }
